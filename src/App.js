f6b27fa747ea1ab4cc3b8d327aede68754594267
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-05 13:42:04 +0800

    initial placeholders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-	return (
-		<div className='App'>
-			<h1>Start here</h1>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>IP Address Tracker</h1>
+      <div>
+        <input />
+        <button>Search</button>
+      </div>
+      <div>
+        <div>
+          <p>IP Address</p>
+          <p>ip</p>
+        </div>
+        <div>
+          <p>Location</p>
+          <p>place</p>
+        </div>
+        <div>
+          <p>Timezone</p>
+          <p>UTC</p>
+        </div>
+        <div>
+          <p>ISP</p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
